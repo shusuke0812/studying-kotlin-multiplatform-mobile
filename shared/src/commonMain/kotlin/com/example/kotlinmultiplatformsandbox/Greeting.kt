bb4bdd64508a9818363e8132a1c842eb44a071be
shusuke0812 <shu.ota0812@gmail.com>
2022-10-29 14:02:52 +0900

    change: shared code of greeting()

--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -1,7 +1,10 @@
 package com.example.kotlinmultiplatformsandbox
 
 class Greeting {
+    private val platform: Platform = Platform()
+
     fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
+        //return "Hello, ${Platform().platform}!"
+        return "Guess what it is! > ${platform.platform.reversed()}"
     }
 }
