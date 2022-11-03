471d2a51096ab65904da57c4ea7a1b7081d71502
shusuke0812 <shu.ota0812@gmail.com>
2022-11-03 13:07:14 +0900

    change: android MainActivity using Compose, add load handling

--- androidGitHubSearchApp/src/main/java/com/example/kotlinmultiplatformsandbox/android/MainActivity.kt
+++ androidGitHubSearchApp/src/main/java/com/example/kotlinmultiplatformsandbox/android/MainActivity.kt
@@ -2,9 +2,46 @@ package com.example.kotlinmultiplatformsandbox.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Surface
 import com.example.kotlinmultiplatformsandbox.Greeting
 import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface (
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                val scope = rememberCoroutineScope()
+                var text by remember { mutableStateOf("Loading") }
+                LaunchedEffect(true) {
+                    scope.launch {
+                        text = try {
+                            Greeting().greeting()
+                        } catch (e: Exception) {
+                            e.localizedMessage ?: "error"
+                        }
+                    }
+                }
+                Text(text = text)
+            }
+        }
+    }
+}
+
+/* old
 fun greet(): String {
     return Greeting().greeting()
 }
@@ -17,4 +54,4 @@ class MainActivity : AppCompatActivity() {
         val tv: TextView = findViewById(R.id.text_view)
         tv.text = greet()
     }
-}
+}*/
