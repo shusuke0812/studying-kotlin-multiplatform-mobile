f3bd21e85608a39b09000669c053aea21817ff98
shusuke0812 <shu.ota0812@gmail.com>
2022-11-03 17:12:50 +0900

    change: iOS ContentView, add load handling

--- iosGitHubSearchApp/iosGitHubSearchApp/ContentView.swift
+++ iosGitHubSearchApp/iosGitHubSearchApp/ContentView.swift
@@ -2,11 +2,28 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+	@ObservedObject private(set) var viewModel: ViewModel = ViewModel()
 
 	var body: some View {
-		Text(greet)
+		Text(viewModel.text)
 	}
+}
+
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var text = "Loading..."
+        init() {
+            Greeting().greeting { greeting, error in
+                DispatchQueue.main.async {
+                    if let greeting = greeting {
+                        self.text = greeting
+                    } else {
+                        self.text = error?.localizedDescription ?? "error"
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
